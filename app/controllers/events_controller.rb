6882fe6bf4c0ca9a2faeae03453bab38f4a9d0f3
zenott <mblevel@gmail.com>
2019-06-04 18:22:34 +0200

    Add create event form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,16 @@
 class EventsController < ApplicationController
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.new(event_params)
+    @event.creator_id = current_user.id
+    if @event.save
+      redirect_to @event
+    else
+      render :new
+    end
   end
 
   def show
@@ -10,4 +18,10 @@ class EventsController < ApplicationController
 
   def index
   end
+
+  private
+
+    def event_params
+      params.require(:event).permit(:name, :description, :date)
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-#      log_in @user
       redirect_to @user
     else
-      render :show
+      render :new
     end
   end
 
