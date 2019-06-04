f78af9f2689d547ccb16b35cc8476cc4178cc7f7
zenott <mblevel@gmail.com>
2019-06-04 13:50:13 +0200

    Add user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,24 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+#      log_in @user
+      redirect_to @user
+    else
+      render :show
+    end
   end
 
   def show
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email)
+    end
 end
