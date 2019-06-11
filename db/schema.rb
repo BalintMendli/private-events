c83221ab4a1568e63fa2d76b946c45fabde974e9
zenott <mblevel@gmail.com>
2019-06-11 13:49:11 +0200

    Add confirmation to attendances

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_184808) do
+ActiveRecord::Schema.define(version: 2019_06_10_201809) do
 
   create_table "attendances", force: :cascade do |t|
     t.integer "attendee_id"
     t.integer "attended_event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "confirmation", default: false
     t.index ["attended_event_id"], name: "index_attendances_on_attended_event_id"
     t.index ["attendee_id"], name: "index_attendances_on_attendee_id"
   end
